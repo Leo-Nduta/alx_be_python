324c3ca8bd9937a65d12a06ab5db734e0d51627d
Leonora Nduta <nduta13mbugua@gmail.com>
2025-09-11 20:10:12 +0300

    Initial commit

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = int(input("Enter integer 1: "))
+number2 = int(input("Enter integer 2: "))
+
+sum_result = number1 + number2
+difference  = number1 - number2
+product = number1 * number2
+
+print("sum of number1 and number2 is", sum_result )
+print("difference of number1 and number2 is", difference)
+print("product of number1 and number2 is", product)

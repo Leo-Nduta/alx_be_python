a3d8bae49df83609a9776da5602d532e5493791c
Leonora Nduta <nduta13mbugua@gmail.com>
2025-09-26 17:18:22 +0300

    "Changed input writing"

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,17 +10,17 @@ def display_menu():
 def main():
     while True:
         display_menu()
-        choice = input(int("Enter your choice: "))
+        choice = input("Enter your choice: ")
         
         if choice == '1':
-            item =  input("Item name:")
+            item =  input("Enter the item to add:")
             shopping_list.append(item)
             # Prompt for and add an item
             print(f"{item} has been added to shopping list")
             
         elif choice == '2':
             # Prompt for and remove an item
-            item =  input("Item name: ")
+            item =  input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"{item} has been removed from shopping list")

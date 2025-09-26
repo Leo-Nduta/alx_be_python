4e2ce2d52986bc606b986e008ec4590c6b599312
Leonora Nduta <nduta13mbugua@gmail.com>
2025-09-26 17:08:29 +0300

    "Shopping list commit"

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,40 @@
+shopping_list = []
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ").strip()
+        
+        if choice == '1':
+            item =  input("Item name:")
+            shopping_list.append(item)
+            # Prompt for and add an item
+            print(f"{item} has been added to shopping list")
+            
+        elif choice == '2':
+            # Prompt for and remove an item
+            item =  input("Item name: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} has been removed from shopping list")
+            else:
+                print(f"{item} not found in shopping list")
+
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()

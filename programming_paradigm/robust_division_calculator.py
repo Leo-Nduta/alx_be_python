63e9edf09a874408065bcde085d54a64ce93c446
Leonora Nduta <nduta13mbugua@gmail.com>
2025-10-05 23:32:38 +0300

    "Robust division calc commit"

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,12 +3,12 @@ def safe_divide(numerator, denominator):
     try:
         num = float(numerator)
         den = float(denominator)
-    except ZeroDivisionError:
-        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."
 
     try:
         result = num / den
     except ValueError:
-        return "Error: Please enter numeric values only."
+        return "Error: Cannot divide by zero."
     
     return("The result of the division is ", result)
